50dadafcb4847f85b1513f2967b7519375661fdd
ios135 <ios135@Mac-11.local>
2017-05-31 19:18:58 +0800

    no message

---
+++ Holland/Holland/FoodPicture.swift
@@ -0,0 +1,35 @@
+//
+//  FoodPicture.swift
+//  Holland
+//
+//  Created by ios135 on 2017/5/22.
+//  Copyright © 2017年 ios135. All rights reserved.
+//
+
+import UIKit
+
+class FoodPicture: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
