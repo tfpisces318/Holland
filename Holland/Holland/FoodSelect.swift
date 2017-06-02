4afa806965ea6f17d1a148ccad548d46a83ea191
Jeff <tfpisces318@gmail.com>
2017-06-02 17:29:29 +0800

    add

--- Holland/Holland/FoodSelect.swift
+++ Holland/Holland/FoodSelect.swift
@@ -8,14 +8,40 @@
 
 import UIKit
 
-class FoodSelect: UIViewController{
+class FoodSelect: UIViewController ,UITableViewDataSource, UITableViewDelegate{
 
+    var arys = ["鍋燒意麵","皮蛋瘦肉粥","水餃","美味餐包"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = arys[indexPath.row]
+        
+        let foodImg:UIImage = UIImage(named: "Food4.jpg")!
+        cell.imageView?.image = foodImg
+        
+        
+//        if indexPath.row == 0 {
+//            cell.detailTextLabel?.text = "bb"
+//        }
+        return cell
         
     }
 
